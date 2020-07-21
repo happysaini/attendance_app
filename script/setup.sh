2ee5d52e62fb6af90f5a735f002f87a1fb67d112
sweet.lady866@gmail.com <sweet.lady866@gmail.com>
2020-07-21 12:22:10 +0530

    remove extra files

--- script/setup.sh
+++ script/setup.sh
@@ -15,10 +15,11 @@ echo "Starting emulator"
 
 # Start emulator in background
 nohup $ANDROID_HOME/emulator/emulator -avd nexus -port 5554 -no-boot-anim -no-snapshot > /dev/null 2>&1 &
-$ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
+$ANDROID_HOME/platform-tools/adb -s emulator-5554 wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
 
 nohup $ANDROID_HOME/emulator/emulator -avd google -port 5556 -no-boot-anim -no-snapshot > /dev/null 2>&1 &
-$ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
+$ANDROID_HOME/platform-tools/adb -s emulator-5556 wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
+
 
 $ANDROID_HOME/platform-tools/adb devices
 export PATH=$PATH:$ANDROID_HOME/platform-tools/
