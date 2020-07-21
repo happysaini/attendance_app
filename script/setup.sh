f8922287ea633be638f9ded5944a8b29c9a94ea9
sweet.lady866@gmail.com <sweet.lady866@gmail.com>
2020-07-21 10:52:17 +0530

    executing parallel

--- script/setup.sh
+++ script/setup.sh
@@ -6,12 +6,16 @@ echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install 'system-images;android-2
 # Create emulator
 echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd -n nexus -k 'system-images;android-27;google_apis;x86' --force
 
+# Create Second emulator
+echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd -n google -k 'system-images;android-27;google_apis;x86' --force
+
 $ANDROID_HOME/emulator/emulator -list-avds
 
 echo "Starting emulator"
 
 # Start emulator in background
 nohup $ANDROID_HOME/emulator/emulator -avd nexus -no-snapshot > /dev/null 2>&1 &
+nohup $ANDROID_HOME/emulator/emulator -avd google -no-snapshot > /dev/null 2>&1 &
 $ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
 
 $ANDROID_HOME/platform-tools/adb devices
